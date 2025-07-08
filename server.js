c5e0e2f75bafc7f6c90d126f12119efa2fc25dd3
khushi11github <khushik110012@gmail.com>
2025-07-08 10:40:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+dotenv.config();
+
+const app = express();
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// mongoose
+//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
+
+  mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.fb1rvlc.mongodb.net/merndb?retryWrites=true&w=majority&appName=Cluster0`)
+
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
+  });
+app.use(express.json());
+app.use("/api/users", userRouter);
