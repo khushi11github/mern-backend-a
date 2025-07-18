a083b79a466a6b05b8c4290812d5c8bbe931cee5
khushi11github <khushik110012@gmail.com>
2025-07-18 08:13:02 +0530

    commit

--- server.js
+++ server.js
@@ -19,22 +19,22 @@ app.use(express.static("public"));
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-mongoose
-  .connect('mongodb://localhost:27017/merncafe',)
+// mongoose
+//   .connect('mongodb://localhost:27017/merncafe',)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
+
+  mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.fb1rvlc.mongodb.net/merndb?retryWrites=true&w=majority&appName=Cluster0`)
+
   .then(() => {
     app.listen(8080, () => {
       console.log("Server started");
     });
   });
-
-
-  // mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.fb1rvlc.mongodb.net/merndb?retryWrites=true&w=majority&appName=Cluster0`)
-
-  // .then(() => {
-  //   app.listen(8080, () => {
-  //     console.log("Server started");
-  //   });
-  // });
 app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
